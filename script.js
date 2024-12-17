e9a190ed1bd95db3e3f3d13ad22ce4e513dcd6f2
Rayhona-web <raykhona.lukmanova@mail.ru>
2024-12-17 16:22:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let hello = prompt('Ismingizni kiriting');
+
+
+
+let abc = prompt('Bironta harf kiriting');
+
+
+
+if(hello.includes(abc)){
+    alert(`${hello} ichida ${abc} harfi mavjud`)
+}
+else{ (hello.includes(abc))
+    alert(`${hello} ichida ${abc} harfi mavjud emas`)
+}
+
